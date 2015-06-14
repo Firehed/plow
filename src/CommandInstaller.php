31ecf3cb568bee3c2e01db4c365fd7db9e99f85b
Eric Stern <eric@ericstern.com>
2015-06-14 16:50:27 -0700

    Build the command trie for the output file

--- src/CommandInstaller.php
+++ src/CommandInstaller.php
@@ -14,6 +14,7 @@ class CommandInstaller extends LibraryInstaller
 
     const COMMAND_DIR = 'plow-commands/';
     const COMMAND_FILE = 'commands.json';
+    const TRIE_VALUE_KEY = '*';
     private $classes = [];
     private $madeChanges = false;
 
@@ -105,9 +106,57 @@ class CommandInstaller extends LibraryInstaller
         foreach ($this->classes as $package => $packageClasses) {
             $classes = array_merge($classes, $packageClasses);
         }
+        $commandMap = self::getCommandMapFromClasses($classes);
+        $trie = self::buildTrieFromCommandMap($commandMap);
         $data['classes'] = $classes;
+        $data['command_trie'] = $trie;
         $data['@gener'.'ated'] = time();
         file_put_contents(self::COMMAND_DIR.self::COMMAND_FILE, json_encode($data));
+    }
+
+    private static function getCommandMapFromClasses(array $classes)
+    {
+        // Ok, so this is sketchy, but there's not a great way around it.
+        // Basically, Composer has dumped the autoloader files, but they're not
+        // actually in-memory for this command. Pull them in so that they can
+        // be loaded and inspected.
+        //
+        // Because this is run from `composer install` (and friends), the CWD
+        // should always be set correctly for this to work as expected.
+        require 'vendor/autoload.php';
+        $commands = [];
+        foreach ($classes as $className) {
+            if (!class_exists($className)) {
+                continue;
+            }
+            $rc = new \ReflectionClass($className);
+            if (!$rc->implementsInterface('Firehed\Plow\CommandInterface')) {
+                continue;
+            }
+            $commandClass = new $className();
+            $commands[$className] = $commandClass->getCommandName();
+        }
+        return $commands;
+    }
+
+    private static function buildTrieFromCommandMap(array $commandMap)
+    {
+        $sub = [];
+        foreach ($commandMap as $className => $command) {
+            $commandWords = explode(' ', strtolower($command));
+            $pos =& $sub;
+            // Index into the output array by word
+            foreach ($commandWords as $word) {
+                if (!isset($pos[$word])) {
+                    $pos[$word] = [];
+                }
+                $pos =& $pos[$word];
+            }
+            $pos[self::TRIE_VALUE_KEY] = $className;
+        }
+        $base['plow'] = $sub;
+        $base['plow'][self::TRIE_VALUE_KEY] = 'Firehed\Plow\Plow';
+        return $base;
     }
 
     private function addCommandsFromPackage(PackageInterface $package)
